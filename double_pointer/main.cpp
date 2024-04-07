241439e011ae7134882ce8a8b5ef010119c03a23
wowns9270 <wowns9270@gmail.com>
2024-04-07 22:37:00 +0900

    aa

---
+++ double_pointer/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+void *get();
+
+struct Man
+{
+    int age;
+    int grade;
+    int score;
+};
+
+void gogo(Man **pptr)
+{
+    Man *tmpPtr = nullptr;
+
+    void *getShm = get(); // shd mem? offset ?? buffer ??
+
+    tmpPtr = static_cast<Man *>(getShm);
+
+    *pptr = tmpPtr;
+}
+
+void *get()
+{
+    Man *ptr = new Man();
+    ptr->age = 10;
+    ptr->grade = 5;
+    ptr->score = 3;
+
+    return static_cast<void *>(ptr);
+}
+
+int main()
+{
+    Man *ptr = nullptr;
+
+    gogo(&ptr);
+
+    std::cout << ptr->age << std::endl;
+    std::cout << ptr->grade << std::endl;
+    std::cout << ptr->score << std::endl;
+}
